dfbb5a156bbe4aee29a6269710d8ebb72a2ce72f
kim kyeong min <kyeongmin21@naver.com>
2023-06-27 13:41:03 +0900

    2.기본 - 배열과 튜플

---
+++ src/section1/chapter2.ts
@@ -0,0 +1,27 @@
+// 배열
+let numArr: number[] = [1, 2, 3];
+let strArr: string[] = ['a', 'b', 'c'];
+let boolArr: boolean[] = [true, true, false];
+
+let boolArr2: Array<boolean> = [true, true, false];
+
+
+// 배열에 들어가는 요소들의 타입이 다양할 경우
+let multiArr: (number | string)[] = [1, 'min'];
+
+// 다차원 배열의 타입을 정의하는 방법
+let doubleArr: number[][] = [
+  [1, 2, 3],
+  [4, 5]
+]
+
+// 튜플 : 길이와 타입이 고정된 배열
+let tup1:[number, number] = [1, 2];
+let tup2:[number, string] = [1, 'min'];
+
+const users: [string, number][] = [
+  ['min', 1],
+  ['jin', 2],
+  ['han', 3],
+  // [4, 'kim'] error
+]
