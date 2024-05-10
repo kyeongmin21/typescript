1caa0e66b989cd3392ac227d87f251a724584ab9
kim kyeong min <kyeongmin21@naver.com>
2024-05-10 12:04:55 +0900

    삭제기능, useReducer로 변경

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,23 +1,43 @@
 import './App.css'
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useEffect, useRef, useReducer} from 'react'
 import Editor from 'components/Editor'
+import TodoItem from 'components/TodoItem'
+import { Todo } from 'types'
 
-interface Todo {
-  id: number;
-  content: string;
+type Action =
+  | { type: 'CREATE'; data: { id: number, content: string }}
+  | { type: 'DELETE'; id: number }
+
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case 'CREATE': {
+      return [...state, action.data]
+    }
+    case 'DELETE': {
+      return state.filter((item) => item.id !== action.id)
+    }
+  }
 }
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todos, dispatch] = useReducer(reducer, [])
   const idRef = useRef(0)
   const onClickAdd = (text: string) => {
-    setTodos([
-      ...todos,
-      {
+    dispatch({
+      type: 'CREATE',
+      data: {
         id: idRef.current++,
         content: text
       }
-    ])
+    })
+  }
+
+  const onDelete = (id: number) => {
+    dispatch({
+      type: 'DELETE',
+      id: id,
+    })
   }
 
   useEffect(() => {
@@ -28,6 +48,11 @@ function App() {
     <div className="App">
       <h1>TodoList</h1>
       <Editor onClickAdd={onClickAdd}/>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} onDelete={onDelete}/>
+        ))}
+      </div>
     </div>
   )
 }
